25f244b61282841bc9231b2c70027a594216e493
Rishi Kaniyawala <rishikaniyawala@gmail.com>
2018-10-18 14:07:58 +1300

    delete data.db

--- app.py
+++ app.py
@@ -12,10 +12,11 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = true
 app.secret_key = 'Rishi'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWT(app, authenticate, identity)j
 
 
 api.add_resource(Store, '/store/<string:name>')
@@ -31,4 +32,8 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
 
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
     app.run(port=5000, debug=True)
