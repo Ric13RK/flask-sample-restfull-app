b51b3b2d1c6da963786ca6237549cdc634bbadd1
Rishi Kaniyawala <rishi@intela.ai>
2020-10-27 00:03:48 +1300

    use jwt extended

--- models/item.py
+++ models/item.py
@@ -1,9 +1,11 @@
 from db import db
 
+
 class ItemModel(db.Model):
     __tablename__ = 'items'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(90))
+    name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
 
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
@@ -14,12 +16,21 @@ class ItemModel(db.Model):
         self.price = price
         self.store_id = store_id
 
-    def in_json(self):
-        return {'name': self.name, 'price': self.price}
+    def json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'store_id': self.store_id
+        }
 
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
 
     def save_to_db(self):
         db.session.add(self)
--- models/user.py
+++ models/user.py
@@ -1,23 +1,22 @@
-import sqlite3
 from db import db
 
+
 class UserModel(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-
-    TABLE_NAME = 'users'
-
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+    def json(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
 
     @classmethod
     def find_by_username(cls, username):
@@ -26,3 +25,11 @@ class UserModel(db.Model):
     @classmethod
     def find_by_id(cls, _id):
         return cls.query.filter_by(id=_id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
